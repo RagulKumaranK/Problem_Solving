25a442f61b8b40d0b80adce75c876171995e13f8
Ragul Kumaran <RagulkumaranK@gmail.com>
2025-08-25 20:39:07 +0530

    made mistake in perfectNUmber

--- BasicProblems/perfectNumberCheck.java
+++ BasicProblems/perfectNumberCheck.java
@@ -2,21 +2,21 @@ package BasicProblems;
 
 public class perfectNumberCheck {
     public static void main(String[] args) {
-        System.out.println(perfectNumberCheckProblem(12));
+        System.out.println(perfectNumberCheckProblem(28));
     }
     static boolean perfectNumberCheckProblem(int x){
         int sum=1;
-        for(int i =2;i<=x/2;i++){
+        System.out.println(Math.sqrt(x));
+        for(int i =2;i<=Math.sqrt(x);i++){
+            
             if(x%i==0){
                 sum+=i;
-                if(i!=x/i){
+                if(i!= x/i){
                     sum+=x/i;
                 }
             }
         }
-        if(sum==x) return true;
-
-        return false;
+        return x==sum;
 
     }
 }
