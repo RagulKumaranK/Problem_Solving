8851758f7bb02d01edaa50cc018dbf3dfc70db1d
Ragul Kumaran <RagulkumaranK@gmail.com>
2025-09-11 20:22:39 +0530

    add comment

--- BasicProblems/Trailing_zero_inFactorial.java
+++ BasicProblems/Trailing_zero_inFactorial.java
@@ -1,5 +1,18 @@
 package BasicProblems;
+/*
+input number ah factorial pannitu like 100! pannitu atha oda output laa evolo
+zeros irukunu solanum atha trailing zero so 100! = 100000000000000000000000000000 = 24 zeros 
 
+epidi vathuchu? just simple:
+100/5 = 20
+20/5 = 4
+so 24.
+
+other method :
+100/5 = 20
+100/25 =4
+so 24
+ */
 public class Trailing_zero_inFactorial {
     public static void main(String[] args) {
         System.out.println(trailingZero(100));
